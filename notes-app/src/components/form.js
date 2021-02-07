2c374f16613c9a41bfe437f472e9a45b61c71c80
Aishahsatouf <aishasattouf1996@gmail.com>
2021-02-07 23:00:26 +0200

    hhh

---
+++ notes-app/src/components/form.js
@@ -0,0 +1,47 @@
+  
+import React, { useState } from 'react';
+import {Form,Button} from 'react-bootstrap';
+
+function TodoForm (props) {
+
+const [item,setItem]=useState({});
+
+ const handleInputChange = e => {
+    setItem({...item,[e.target.name]: e.target.value });
+  };
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    props.handleSubmit(item);
+    const newitem = {};
+    setItem(newitem);
+  };
+
+  
+    return (
+      <>
+        <h3>Add Item</h3>
+       <div>
+        <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>To Do Item</Form.Label>
+          <Form.Control  onChange={handleInputChange} type="text"  name="text" placeholder="Add To Do List Item" />
+        </Form.Group>
+        <Form.Group controlId="formBasicRange">
+          <Form.Label>Difficulty Rating</Form.Label>
+          <Form.Control type="range" />
+        </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Assigned To</Form.Label>
+          <Form.Control  onChange={handleInputChange} type="text"  name="assignee" placeholder="Assigned To" />
+        </Form.Group>
+        <Button variant="primary" type="submit"> Add Item</Button>
+      </Form>
+      </div>
+      </>
+    );
+  }
+
+
+export default TodoForm;
